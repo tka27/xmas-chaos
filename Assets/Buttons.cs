544873ebdf2c179f9eedf522c17cda9bbe67fc11
KorNorJb <najomob@gmail.com>
2021-11-16 23:53:15 +0600

    Two Buttons
    Ну тут две кнопочки(Start and Settings)

---
+++ Assets/Buttons.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+    public GameObject menu;
+    public GameObject settingsmenu;
+
+   public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Settings()
+    {
+        menu.SetActive(false);
+        settingsmenu.SetActive(true);
+    }
+}
