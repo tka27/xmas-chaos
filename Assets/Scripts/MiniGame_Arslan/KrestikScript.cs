2ed2407b29804f3e5279e6f51570afd5aae67182
KorNorJb <najomob@gmail.com>
2021-11-27 00:14:34 +0600

    Крестик
    В голове сложилось как это сделать, но на деле не сложилось. Завтра буду думоть:D(За то добавил спрайт Деда Максима)

--- Assets/Scripts/MiniGame_Arslan/KrestikScript.cs
+++ Assets/Scripts/MiniGame_Arslan/KrestikScript.cs
@@ -12,7 +12,7 @@ public class KrestikScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape))
         {
 
-
+        
             if (GamePaused)
             {
                 Resume();
