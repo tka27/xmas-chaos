184e5a14cfcf90d387d070c7ed0e1d11b4311fd5
KorNorJb <najomob@gmail.com>
2021-12-04 02:53:48 +0600

    Марафет с локализацией

--- Assets/Scripts/VolumeValue.cs
+++ Assets/Scripts/VolumeValue.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class VolumeValue : MonoBehaviour
 {
     private AudioSource audioSrc;
+    private AudioSource effetsSrc;
     private float musicVolume = 1f;
 
     void Start()
     {
         audioSrc = GetComponent<AudioSource>();
+        effetsSrc = GetComponent<AudioSource>();
+        
     }
 
     void Update()
     {
         audioSrc.volume = musicVolume;
+        effetsSrc.volume = musicVolume;
     }
 
     public void SetVolume(float vol)
