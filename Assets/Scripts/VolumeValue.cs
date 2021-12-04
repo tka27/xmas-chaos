846eef72b2bd5de33a4e98cabf6ab6436ab68863
KorNorJb <najomob@gmail.com>
2021-12-04 15:46:12 +0600

    Sounds

--- Assets/Scripts/VolumeValue.cs
+++ Assets/Scripts/VolumeValue.cs
@@ -1,28 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VolumeValue : MonoBehaviour
 {
     private AudioSource audioSrc;
-    private AudioSource effetsSrc;
     private float musicVolume = 1f;
 
     void Start()
     {
         audioSrc = GetComponent<AudioSource>();
-        effetsSrc = GetComponent<AudioSource>();
-        
+        if (PlayerPrefs.HasKey("Volume"))
+        {
+            PlayerPrefs.GetFloat("Volume", musicVolume);
+        }
+        else
+        {
+            audioSrc = GetComponent<AudioSource>();
+        }
     }
-
     void Update()
     {
         audioSrc.volume = musicVolume;
-        effetsSrc.volume = musicVolume;
     }
-
     public void SetVolume(float vol)
     {
         musicVolume = vol;
+        PlayerPrefs.SetFloat("Volume", audioSrc.volume);
     }
+
 }
