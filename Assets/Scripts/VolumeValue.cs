b2c54b6b3db7996ca699304b2cebe02db3b317cf
KorNorJb <najomob@gmail.com>
2021-12-05 22:47:21 +0600

    SaveSoundSettings

--- Assets/Scripts/VolumeValue.cs
+++ Assets/Scripts/VolumeValue.cs
@@ -7,36 +7,17 @@ public class VolumeValue : MonoBehaviour
 {
     private AudioSource audioSrc;
     private float musicVolume = 1f;
-
     void Start()
     {
-
-        if (!PlayerPrefs.HasKey("Volume"))
-        {
-            PlayerPrefs.SetFloat("Volume", musicVolume);
-            Load();
-        }
-        else
-        {
-            audioSrc = GetComponent<AudioSource>();
-        }
+        audioSrc = GetComponent<AudioSource>();
+        
     }
     void Update()
     {
         audioSrc.volume = musicVolume;
     }
-    private void Load()
-    {
-       musicVolume = PlayerPrefs.GetFloat("Volume");
-    }
     public void SetVolume(float vol)
     {
         musicVolume = vol;
-        Save();
     }
-    private void Save()
-    {
-        PlayerPrefs.SetFloat("Volume", musicVolume);
-    }
-
 }
